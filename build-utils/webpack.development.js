a50a5184c3b1f12cadc68ac7149fdaaceffae342
Alexa Cristina <alexa@maptheclouds.com>
2021-10-19 12:06:23 +0300

    feat: Optimize for production: a11y, service workers

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -6,7 +6,7 @@ module.exports = () => ({
   mode: 'development',
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
     assetModuleFilename: 'icons/[name][ext]',
   },
 
--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,6 +1,9 @@
 const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = () => ({
   mode: 'production',
@@ -8,10 +11,12 @@ module.exports = () => ({
   output: {
     assetModuleFilename: 'icons/[name][ext]',
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
 
-  devtool: 'source-map',
+  optimization: {
+    minimizer: [new TerserPlugin({}), new CssMinimizerPlugin()],
+  },
 
   module: {
     rules: [
@@ -29,8 +34,30 @@ module.exports = () => ({
       filename: '[name].css',
       chunkFilename: '[id].css',
     }),
-    new UglifyJSPlugin({
-      uglifyOptions: { warnings: false },
+    new WorkboxPlugin.GenerateSW({
+      // Do not precache images
+      exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+      // Define runtime caching rules.
+      runtimeCaching: [
+        {
+          // Match any request that ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+          // Apply a cache-first strategy.
+          handler: 'CacheFirst',
+
+          options: {
+            // Use a custom cache name.
+            cacheName: 'images',
+
+            // Only cache 10 images.
+            expiration: {
+              maxEntries: 10,
+            },
+          },
+        },
+      ],
     }),
   ],
 });
